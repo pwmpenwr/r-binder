7a3c7a642d8813cf428c3819f12ef5f4edf00f64
pwmpenwr <pete@interruptroutine.co.uk>
2019-04-13 20:39:00 +0100

    Neural networks in R.

---
+++ neural_net.R
@@ -0,0 +1,39 @@
+## Fit neural network 
+
+# install library
+# install.packages("neuralnet")
+
+# load library
+library(neuralnet)
+
+## Creating index variable 
+
+# Read the Data
+data = read.csv("cereals.csv", header=T)
+
+# Random sampling
+samplesize = 0.60 * nrow(data)
+set.seed(80)
+index = sample( seq_len ( nrow ( data ) ), size = samplesize )
+
+# Create training and test set
+datatrain = data[ index, ]
+datatest = data[ -index, ]
+
+## Scale data for neural network
+
+max = apply(data , 2 , max)
+min = apply(data, 2 , min)
+scaled = as.data.frame(scale(data, center = min, scale = max - min))
+
+# creating training and test set
+trainNN = scaled[index , ]
+testNN = scaled[-index , ]
+
+# fit neural network
+set.seed(2)
+NN = neuralnet(rating ~ calories + protein + fat + sodium + fiber, trainNN, hidden = 3 , linear.output = T )
+
+# plot neural network
+plot(NN)
+
