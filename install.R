15f8ce94aae41ceb6a84d17559dcaef82ff275c3
pwmpenwr <pete@interruptroutine.co.uk>
2019-04-13 20:48:11 +0100

    Add the container install script.

---
+++ install.R
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------------------------
+# install.R
+# Install packages when the container is built so that the code will run.
+# -----------------------------------------------------------------------------
+
+install.packages("neuralnet")
